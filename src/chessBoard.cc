febe1a6121066fdd33027abfc15d6f39e81b5582
VizXu <xujiweigo@163.com>
2016-05-11 01:17:01 +0800

    Update chessBoard.cc

--- src/chessBoard.cc
+++ src/chessBoard.cc
@@ -1,9 +1,20 @@
 #include"chessBaord.h"
+#include<cstdio>
 void ChessBaord::init()
 {
   for(int i=0;i<BOARDSIZE;i++)
   {
     for(int j=0;j<BOARDSIZE;j++)
     chessBaord[i][j]='+';
+  }
+}
+
+void ChessBoard::displayChessBoard()
+{
+  for(int i=0;i<BOARDSIZE;i++){
+    for(int j=0;j<BOARDSIZE;j++){
+      printf("%2c",chessBoard[i][j]);
+    }
+      puts("");
   }
 }
