34fdfab6f374b7809a55e302049b7f2efdf966a8
VizXu <xujiweigo@163.com>
2016-05-19 00:51:14 +0800

    Update chessBoard.h

--- src/chessBoard.h
+++ src/chessBoard.h
@@ -6,6 +6,8 @@ class ChessBoard
 private:
   char chessboard[BAORDSIZE][BAORDSIZE];
 private:
+  friend class Player;//this is the issue!!!
+private:
   void init();//initial the chess baord
 public:
   void displayChessBoard();//show the chess baord
