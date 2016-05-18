85042a383e694d8e1ce51527648a370359b66bfb
VizXu <xujiweigo@163.com>
2016-05-19 00:49:29 +0800

    Update player.h

--- src/player.h
+++ src/player.h
@@ -1,8 +1,22 @@
 #ifndef _PLAYER_
 #define _PLAYER_
+#include<stirng>
+using std::string;
 class Player
 {
 private:
+   int gold;
+   string name;
+private:
    
+public：
+   string getName()
+   {
+      return this->name;
+   }
+   int getGold()
+   {
+      return this->gold;
+   }
 };
 #endif//player.h
