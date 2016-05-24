b1e0e716e54eaa44177c7610f39e6f2144fc2425
VizXu <xujiweigo@163.com>
2016-05-25 00:10:50 +0800

    Update player.cc

--- src/player.cc
+++ src/player.cc
@@ -1,6 +1,7 @@
 #include"player.h"
-void Player::setPosition(int _x,int _y)
+void Player::setPosition(int _x,int _y,char ch,Chessboard& chessboard)
 {
   this->posX=_x;
   this->posY=_y;
+  chessBoard[_x][_y]=ch;
 }
