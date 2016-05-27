14868aae6ea660f535787696ee84f535cd001fcc
VizXu <xujiweigo@163.com>
2016-05-27 19:40:03 +0800

    Update player.cc

--- src/player.cc
+++ src/player.cc
@@ -1,7 +1,11 @@
 #include"player.h"
+#include<iostream>
 void Player::setPosition(int _x,int _y,char ch,Chessboard& chessboard)
 {
   this->posX=_x;
   this->posY=_y;
-  chessBoard[_x][_y]=ch;
+  if(chessboard.isLegal(_x,_y))
+      chessBoard[_x][_y]=ch;
+  else
+    std::cout<<"the position is not legal,please set other position!"<<std::endl;
 }
