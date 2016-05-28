400214609e4c4e48439d41fc86cd7c406578cfcd
VizXu <xujiweigo@163.com>
2016-05-28 18:54:02 +0800

    Update chessBoard.h

--- src/chessBoard.h
+++ src/chessBoard.h
@@ -1,17 +1,18 @@
 #ifndef _CHESSBOARD_H
 #define _CHESSBOARD_H
-#define BAORDSIZE 10
+#define BOARDSIZE 10
 #include"player.h"
 class ChessBoard
 {
 private:
-  char chessboard[BAORDSIZE][BAORDSIZE];
+  char chessBoard[BOARDSIZE][BOARDSIZE];
   int  xPosition,yPosition;
 private:
   friend void Player::setPosition(int _x,int _y,char ch,ChessBoard& chessboard);//this is the friend function
 private:
   bool isLegal(int _x,int _y);
   void init();//initial the chess baord
+  void setPos(int _x,int _y,char ch);
 public:
   ChessBoard(){
     init();
