fca326d68b05aa3a461991dd6361448cd732f2e0
VizXu <xujiweigo@163.com>
2016-05-28 18:57:41 +0800

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,15 @@
+#include<iostream>
 #include"chessBoard.h"
 #include"player.h"
+using std::cout;
+using std::endl;
 int main()
 {
-  
+  ChessBoard chessBoard;
+  Player player;
+  chessBoard.displayChessBoard();
+  player.setPosition(2,3,'#',chessBoard);
+  cout<<"-----------------"<<endl;
+  chessBoard.displayChessBoard();
 return 0;
 }
