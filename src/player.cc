72ca300634bd81ea59d6c7aa7c2270153582ab85
VizXu <xujiweigo@163.com>
2016-05-29 19:43:06 +0800

    Update player.cc

--- src/player.cc
+++ src/player.cc
@@ -5,8 +5,11 @@ void Player::setPosition(int _x,int _y,char ch,ChessBoard& chessboard)
 {
   this->posX=_x;
   this->posY=_y;
-  if(chessboard.isLegal(_x,_y))
-      chessboard.setPos(_x,_y,ch);
+  if(chessboard.isLegal(_x,_y)){
+       chessmanNumber++;
+       chessboard.setPos(_x,_y,ch);
+       recordChessmanInfo();
+  }
   else
     std::cout<<"the position is not legal,please set other position!"<<std::endl;
 }
