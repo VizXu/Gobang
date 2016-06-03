db1e86b39ee7497fb3a4b5322bc30b42e3cd3ff6
VizXu <xujiweigo@163.com>
2016-06-03 13:32:00 +0800

    Update person.h

--- src/person.h
+++ src/person.h
@@ -1,9 +1,17 @@
 #ifndef _PERSON_
 #define _PERSON_
+#include<string>
 #include"player.h"
+using std::string;
 class Person:public
 {
 private:
+  string name;
+public:
+  Person(){}
+  Person(string n="Zhangsan"):Player(n){}
+  ~Person(){}
+public:
   
 };
 #endif//person.h
