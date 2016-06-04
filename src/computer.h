5f2fe9f6583ccb96f9c3ca953e421b726cb9db37
VizXu <xujiweigo@163.com>
2016-06-04 12:20:20 +0800

    Update computer.h

--- src/computer.h
+++ src/computer.h
@@ -1,8 +1,15 @@
 #ifndef _COMPUTER_
 #define _COMPUTER_
 #include"player.h"
+#include<string>
+typedef unsigned int U32;
 class Computer:public Player
 {
-
+private:
+  U32 levels;
+  string name;
+public:
+  string getName();
+  U32 getLevel();
 };
 #endif//computer.h
