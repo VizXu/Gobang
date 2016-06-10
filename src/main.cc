1828ef7b13b90ccdb5fee949dae4bd45a2b97e64
木兰宿莽 <xujiweigo@163.com>
2016-06-11 01:30:51 +0800

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -3,13 +3,9 @@
 #include"person.h"
 using std::cout;
 using std::endl;
+int run();
 int main()
 {
-  ChessBoard chessBoard;
-  Person person;
-  chessBoard.displayChessBoard();
-  person.play(2,3,'#',chessBoard);
-  cout<<"-----------------"<<endl;
-  chessBoard.displayChessBoard();
+  run();
 return 0;
 }
