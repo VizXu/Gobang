d1f894bce4f85cc57f2a04ca6630603438373f83
木兰宿莽 <xujiweigo@163.com>
2016-06-10 21:03:44 +0800

    Update run.cc

--- src/run.cc
+++ src/run.cc
@@ -2,6 +2,7 @@
 #include<string>
 #include"person.h"
 #include"computer.h"
+#include"chessBoard.h""
 using std::cout;
 using std::endl;
 using std::cin;
@@ -9,15 +10,29 @@ using std::string;
 int run()
 {
 //start the game
-  flag1:
   string playerName;
   string computName;
   cout<<"----start----"<<endl;
+  flag1:
   cout<<"Please put in you name:"<<endl;
   cin>>playerName;
   if(!cin.good()){
     cout<<"Input error,Please put in again"<<endl;
+    cin.clear();
     while(cin.get()!='\n')continue;
     goto flag1;
   }
+  flag2:
+  cout<<"Please put in the computer's name:"<<endl;
+  cin>>computName;
+  if(!cin.good()){
+    cout<<"Input error,Please put in again"<<endl;
+    cin.clear();
+    while(cin.get()!='\n')continue;
+    goto flag2;
+  }
+  cout<<"Good,start the chess..."<<endl;
+  ChessBoard chessboard;
+  Person person(playerName);
+  Computer computer(computName);
 }
