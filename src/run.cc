120a1f35ddfd473289cbc2ca88de9d72be13b39c
木兰宿莽 <xujiweigo@163.com>
2016-06-10 20:53:56 +0800

    Update run.cc

--- src/run.cc
+++ src/run.cc
@@ -1,7 +1,23 @@
+#include<iostream>
+#include<string>
 #include"person.h"
 #include"computer.h"
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
 int run()
 {
 //start the game
-
+  flag1:
+  string playerName;
+  string computName;
+  cout<<"----start----"<<endl;
+  cout<<"Please put in you name:"<<endl;
+  cin>>playerName;
+  if(!cin.good()){
+    cout<<"Input error,Please put in again"<<endl;
+    while(cin.get()!='\n')continue;
+    goto flag1;
+  }
 }
