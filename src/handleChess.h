a8ff4aadb8b177d0039e49b1f3f1bc05d798bb55
木兰宿莽 <xujiweigo@163.com>
2016-06-29 09:35:32 +0800

    Create handleChess.h

---
+++ src/handleChess.h
@@ -0,0 +1,14 @@
+#ifndef _HANDLECHESS_
+#define _HANDLECHESS_
+#include"chessBoard.h"
+#include"types.h"
+class HandleChessBoard
+{
+private:
+  int _xPosition;
+  int _yPosition;
+  U8 levels;
+public:
+  
+};
+#endif//handleChess.h
