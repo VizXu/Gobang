c83d5b394e9c241f2a7721f448373087e8e2d9c5
木兰宿莽 <xujiweigo@163.com>
2016-07-10 19:53:28 +0800

    Update handleChess.cc

--- src/handleChess.cc
+++ src/handleChess.cc
@@ -35,7 +35,20 @@ void HandleChessBoard::handleChessBoard(const ChessBoard& chessboard)
   for(int i=0;i<BOARDSIZE;++i){
     for(int j=0;j<BOARDSIZE;++j){
       switch(chessboard.getChessman(i,j)){
-        case('+'):;
+        case('+'):
+          Position position(i,j);
+          vector<Position> blankPosition.push_back(std::move(position));
+        break;
+        case('&'):
+          Position position(i,j);
+          vector<Position> playerPosition.push_back(std::move(position));
+          break;
+        case('#'):
+          Position position(i,j);
+          vector<Position> computPosition.push_back(std::move(position));
+          break;
+        default:
+          throw "error chessman";
       }
     }
   }
