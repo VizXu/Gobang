d9fe24e3e45c0b6d8e61c81cc011249ee5b483f8
木兰宿莽 <xujiweigo@163.com>
2016-07-10 20:01:22 +0800

    Update handleChess.h

--- src/handleChess.h
+++ src/handleChess.h
@@ -2,13 +2,33 @@
 #define _HANDLECHESS_
 #include"chessBoard.h"
 #include"types.h"
+#include<vector>
+using std::vector;
 class ChessBoard;
+
+class Position
+{
+private:
+  int xPos;
+  int yPos;
+public:
+  Position(int i=0,int j=0):xPos(i),yPos(j){}
+  ~Position(){}
+public:
+  int getxPos(){return this->xPos;}
+  int getyPos(){return this->yPos;}
+};
 class HandleChessBoard
 {
 private:
   int xChessman;
   int yChessman;
   U8 levels;
+private:
+  vector<Position> blankPosition;
+  vector<Position> playerPosition;
+  vector<Position> computPosition;
+  vector<Position>::iterator iPtr;
 public:
   void setLevel(U8 levels);
   int getXChessman();
