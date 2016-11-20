40cad4affe1bb8dee47dc958ff4b49280434e201
木兰宿莽 <xujiweigo@gmail.com>
2016-11-20 20:36:49 +0800

    Update judgedegree.cc
    no message

--- src/judgedegree.cc
+++ src/judgedegree.cc
@@ -1,1 +1,20 @@
+#include"chessBoard.h"
 
+void copyChessBoard(char chessBoard[BOARDSIZE][BOARDSIZE],ChessBoard& chessboard)
+{
+  for(int i=0;i<BOARDSIZE;i++){
+    for(int j=0;j<BOARDSIZE;j++){
+     chessBoard[i][j]='+';
+    }
+  }
+  for(int i=0;i<BOARDSIZE;i++){
+    for(int j=0;j<BOARDSIZE;j++){
+     chessBoard[i][j]=chessboard.getChessman(i,j);
+    }
+  }
+}
+
+void judgeValueDegree(char chessBoard[BOARDSIZE][BOARDSIZE])
+{
+
+}
