3c49875d41c03c6acb87cca28663d362597d33b9
木兰宿莽 <xujiweigo@gmail.com>
2017-02-08 20:50:00 +0800

    correct the types.h
    no message

--- CoreEngine/types.h
+++ CoreEngine/types.h
@@ -1,7 +1,7 @@
 #indef _TYPES_H_
 #define _TYPES_H_
-typedef u8 unsigned char;
-typedef s8 signed char;
-typedef u32 unsigned int;
-typedef s32 signed int;
+typedef unsigned char u8 ;
+typedef signed char s8;
+typedef unsigned int u32 ;
+typedef  signed int s32;
 #endif// types.h
