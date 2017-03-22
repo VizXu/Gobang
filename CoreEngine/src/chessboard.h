192dc697613a55b05a242e7812e5c6eee333a83b
木兰宿莽 <xujiweigo@gmail.com>
2017-03-22 14:41:37 +0800

    update the chessboard.h
    no message

--- CoreEngine/src/chessboard.h
+++ CoreEngine/src/chessboard.h
@@ -1,30 +1,34 @@
 #ifndef CHESSBOARD_H_
 #define CHESSBOARD_H_
 
-#include "../types.h"
+#include "types.h"
 #define BOARD_SIZE 8
 
-class ChessBoard
+
+class Chessboard
 {
 private:
-  s8 chessboard[BOARD_SIZE][BOARD_SIZE];
-private:
-  void set_chess(u32,u32);
+    s8 board[BOARD_SIZE][BOARD_SIZE];
 public:
-  u32 total_empty_site();
-  bool is_empty_site(u32,u32);
-public:
-  ChessBoard(){
-    for(int i=0;i<BOARD_SIZE;i++){
-      for(int j=0;j<BOARD_SIZE;j++){
-         this->chessboard[i][j] = '+';
+    Chessboard(){
+      for(int i = 0; i < BOARD_SIZE; i++){
+        for(int j = 0;j < BOARD_SIZE; j++){
+          board[i][j] = '+';
+        }
       }
     }
-  }
-  ~ChessBoard(){}
+    ~Chessboard(){}
+    Chessboard(const Chessboard&) = delete;
+    Chessboard& operator=(const Chessboard&) = delete;
+    Chessboard& operator()(const Chessboard&) = delete;
 public:
-  ChessBoard& operator()(const ChessBoard&) = delete;
-  ChessBoard(const ChessBoard&) = delete;
-  ChessBoard& operator=(const ChessBoard&) = delete;
+private:
+    u32 scan_board() const;
+    bool is_board_empty() const;
+private:
+    bool is_site_empty(u32,u32) const;
+public:
+    bool set_chess(u32,u32,s8);
+    s8   get_chess(u32,u32) const;
 };
-#endif //chessboard.h
+#endif//chessboard.h
