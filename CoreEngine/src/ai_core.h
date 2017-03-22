ec128c6a43f30bc2247572911b3a802d3a89c007
木兰宿莽 <xujiweigo@gmail.com>
2017-03-22 16:43:53 +0800

    add the ai_core.h
    no message

---
+++ CoreEngine/src/ai_core.h
@@ -0,0 +1,66 @@
+#ifndef _AI_CORE_H
+#define _AI_CORE_H
+
+#include "chessboard.h"
+#include "types.h"
+#include <ctime>
+#include <cstdlib>
+#include <list>
+#include <vector>
+
+struct board_position
+{
+  int x_pos;
+  int y_pos;
+};
+
+typedef struct{
+      board_position position;
+      u32 direction;
+      u32 max_length;
+}analysis_result;
+
+class AI_core
+{
+private:
+   analysis_result alys_rlt;
+   s8 core_board[BOARD_SIZE][BOARD_SIZE];
+   board_position position_suggest;
+   std::list<board_position> type1;
+   std::list<board_position> type2;
+   std::list<board_position> empty_type;
+   std::list<board_position>::iterator l_ptr;
+   std::vector<std::vector<int>> suggest_position_info;/* 0-horizontal 1-vertical 2-hori_vert 3-anti_hori_vert*/
+public:
+   AI_core(){
+     srand(time(NULL));
+     position_suggest.x_pos = BOARD_SIZE/2;
+     position_suggest.y_pos = BOARD_SIZE/2;
+
+     this->alys_rlt.direction = 0;
+     this->alys_rlt.max_length = 0;
+     for(int i = 0; i < BOARD_SIZE; i++){
+       for(int j = 0; j < BOARD_SIZE; j++){
+         core_board[i][j] = '+';
+       }
+     }
+   }      
+   ~AI_core(){}
+public:
+   AI_core(const AI_core&)= delete;
+   AI_core& operator=(const AI_core&) = delete;
+   AI_core& operator()(const AI_core&) = delete;
+public:
+   void copy_board(const Chessboard&);
+   board_position get_suggest_position();
+private:
+   void store_chess_info();    
+   void destroy_present_chess_info();
+   analysis_result  analysize_guess_position(const board_position&,s8);
+   typedef s8 COPY_BOARD[BOARD_SIZE][BOARD_SIZE];
+   void copy_position(COPY_BOARD&);
+   void store_analysis_result(const analysis_result&);
+public:
+   void analyze_level1(s8);
+};
+#endif//ai_core.h
