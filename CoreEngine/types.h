f1c1135bde1acb8031f900cc28d4a0dcbf5c0cb2
木兰宿莽 <xujiweigo@gmail.com>
2017-03-23 18:52:07 +0800

    delete the types.h
    no message

--- CoreEngine/types.h
+++
@@ -1,9 +0,0 @@
-#indef _TYPES_H_
-#define _TYPES_H_
-typedef unsigned char u8 ;
-typedef signed char s8;
-typedef unsigned int u32 ;
-typedef  signed int s32;
-typedef signed long s64;
-typedef unsigned long u64;
-#endif// types.h
