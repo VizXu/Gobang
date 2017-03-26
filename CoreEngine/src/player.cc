85fd6c2dddaf9fd78885f954727df70cfcddde8a
木兰宿莽 <xujiweigo@gmail.com>
2017-03-26 14:18:50 +0800

    create the player.cc
    no message

---
+++ CoreEngine/src/player.cc
@@ -0,0 +1,43 @@
+#include "player.h"
+
+Player::Player(const string& s):player_name(s)
+{
+  
+  start_time = time(NULL);
+  end_time = time(NULL);
+  use_time = end_time - start_time;
+  move_state = stop;
+  present_position.x_pos = 0;
+  present_position.y_pos = 0;
+  last_position.x_pos = 0;
+  last_position.y_pos = 0;
+  suggest_position.x_pos = 0;
+  suggest_position.y_pos = 0;
+}
+
+Player::~Player()
+{
+
+}
+
+board_position Player::get_present_position()
+{
+   return this->present_position;
+}
+
+bool Player::make_a_step(Chessboard& chessboard)
+{
+     if(this->player_name == "computer")
+       return chessboard.set_chess(this->suggest_position.x_pos,this->suggest_position.y_pos,'@');
+     else return false;
+}
+
+void Player::analysis(Chessboard& chessboard,AI_core& ai,u32 level)
+{
+  // none compeletion
+}
+
+string Player::get_player_name()
+{
+  return this->player_name;
+}
