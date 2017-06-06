efef3c6a89983eefc27460b3aa0c1d5cc90b5f46
VizXu <xujiweigo@163.com>
2017-06-06 22:31:50 +0800

    [owner:jiwei.xu] move the head files to headers

---
+++ CoreEngine/src/headers/boardposition.h
@@ -0,0 +1,9 @@
+#ifndef BOARD_POSITION_
+#define BOARD_POSITION_
+
+struct board_position
+{
+  int x_pos;
+  int y_pos;
+};
+#endif//boardposition.h
---
+++ CoreEngine/src/headers/types.h
@@ -0,0 +1,8 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+typedef char s8;
+typedef int  s32;
+typedef long s64;
+typedef unsigned int u32;
+typedef unsigned long u64;
+#endif //types.h
